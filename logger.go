97d19b8a434c0e8317e22e8dd6ea5f73e7e3cf4e
Andrei Samuilik <a.samuylik@gmail.com>
2021-06-17 12:58:34 +0300

    Logger fix, concurrent use has been implemented

--- logger.go
+++ logger.go
@@ -1,29 +1,36 @@
 package job
 
+import "sync"
+
 // Log record handler
-type logHandler func(...interface{})
+type LogHandler func(...interface{})
 type logHandlerMapItem struct {
 	toggle  bool
-	handler logHandler
+	handler LogHandler
+	mu      sync.Mutex
 }
 
 var (
 	logHandlersMap  = map[interface{}]*logHandlerMapItem{}
-	logDummyHandler = func(...interface{}) { }
+	logDummyHandler = func(...interface{}) {}
 )
 
-func RegisterLogger(kind interface{}, handler logHandler, on bool) {
-	logHandlersMap[kind] = &logHandlerMapItem{
-		toggle:  on,
-		handler: handler,
+func RegisterLogger(kind interface{}, handler LogHandler, on bool) {
+	item := &logHandlerMapItem{}
+	item.toggle = on
+	item.handler = func(args...interface{}) {
+		item.mu.Lock()
+		defer item.mu.Unlock()
+		handler(args...)
 	}
+	logHandlersMap[kind] = item
 }
 
 func ToggleLogger(kind interface{}, on bool) {
 	logHandlersMap[kind].toggle = on
 }
 
-func Logger(kind interface{}) logHandler {
+func Logger(kind interface{}) LogHandler {
 	item := logHandlersMap[kind]
 	if item.toggle {
 		return item.handler
